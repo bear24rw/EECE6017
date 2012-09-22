04b0d1cb7ca3507fee0d6f3fb5d1ea314ebcb2da
Max Thrun <bear24rw@gmail.com>
2012-09-22 15:38:04 -0400

    project dir reorg

--- project_1/simulation/modelsim/rtl_work/clk_div/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity clk_div is
-    port(
-        clk_50mhz       : in     vl_logic;
-        clk_1hz         : out    vl_logic
-    );
-end clk_div;
--- project_1/verilog/tb/work/monitor_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity monitor_tb is
-end monitor_tb;
