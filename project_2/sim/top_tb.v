bb0c73378422ff389ea357bca9a596ad684e5abf
Max Thrun <bear24rw@gmail.com>
2012-10-08 13:54:10 -0400

    retabbed to spaces

--- project_2/sim/top_tb.v
+++ project_2/sim/top_tb.v
@@ -22,51 +22,51 @@
 `include "constants.h"
 
 module top_tb();
-	
-		reg CLOCK_50 = 0;
-		reg [3:0] KEY = 1;
-		reg [9:0] SW;
-		wire [7:0] LEDG;
-		wire [9:0] LEDR;
-		wire [6:0] HEX0;
-		wire [6:0] HEX1;
-		wire [6:0] HEX2;
-		wire [6:0] HEX3;
-		
-		
-		top uut(
-			.CLOCK_50(CLOCK_50),
-			.KEY(KEY),
-			.SW(SW),
-			.LEDG(LEDG),
-			.LEDR(LEDR),
-			.HEX0(HEX0),
-			.HEX1(HEX1),
-			.HEX2(HEX2),
-			.HEX3(HEX3)
-			);
-		
-		initial begin
-			$dumpfile("top_tb.vcd");
-			$dumpvars(0, top_tb);
-		end
-		
-		
-		integer i;
-		
-		initial begin
-		
-		// reset the system
-		reset;
-		
-		// Start the random number generator
-		#100 start_stop;
-		
-		for (i=0; i<1000; i = i+1) begin
-		    #1 CLOCK_50 = 1;
-		    #1 CLOCK_50 = 0;
-		
-			$write("%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d",
+    
+    reg CLOCK_50 = 0;
+    reg [3:0] KEY = 1;
+    reg [9:0] SW;
+    wire [7:0] LEDG;
+    wire [9:0] LEDR;
+    wire [6:0] HEX0;
+    wire [6:0] HEX1;
+    wire [6:0] HEX2;
+    wire [6:0] HEX3;
+        
+        
+    top uut(
+        .CLOCK_50(CLOCK_50),
+        .KEY(KEY),
+        .SW(SW),
+        .LEDG(LEDG),
+        .LEDR(LEDR),
+        .HEX0(HEX0),
+        .HEX1(HEX1),
+        .HEX2(HEX2),
+        .HEX3(HEX3)
+    );
+    
+    initial begin
+        $dumpfile("top_tb.vcd");
+        $dumpvars(0, top_tb);
+    end
+        
+        
+    integer i;
+        
+    initial begin
+
+        // reset the system
+        reset;
+        
+        // Start the random number generator
+        #100 start_stop;
+        
+        for (i=0; i<1000; i = i+1) begin
+            #1 CLOCK_50 = 1;
+            #1 CLOCK_50 = 0;
+
+            $write("%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d",
                 uut.rand_value,
                 uut.div_value,
                 uut.sum_value,
@@ -79,27 +79,28 @@ module top_tb();
                 uut.sum.value_1,
                 uut.sum.value_2,
             );
-		end
-		
-		// stop the generator and the test
-		#100 start_stop;
-		
-		#1000;
-		$finish;
-	end
-			
-			
-		// pulses the reset key
-		task reset; begin
-			#50 KEY[0] = 0; #50 KEY[0] = 1;
-		end
-		endtask
-		
-		
-		// pulses the start/stop key
-		task start_stop; begin
-			#50 KEY[3] = 0; #50 KEY[3] = 1;
-		end
-		endtask	
-		
+        end
+        
+        // stop the generator and the test
+        #100 start_stop;
+        
+        #1000;
+        $finish;
+
+    end
+            
+            
+    // pulses the reset key
+    task reset; begin
+        #50 KEY[0] = 0; #50 KEY[0] = 1;
+    end
+    endtask
+
+
+    // pulses the start/stop key
+    task start_stop; begin
+        #50 KEY[3] = 0; #50 KEY[3] = 1;
+    end
+    endtask 
+        
 endmodule
