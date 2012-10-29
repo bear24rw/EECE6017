19f413b24b84c245fa7687f7cb0f31c00fad1ec1
Max Thrun <bear24rw@gmail.com>
2012-10-29 12:00:29 -0400

    [code] retab

--- readers_writers_lab/code/readers_writers/reader_writer.c
+++ readers_writers_lab/code/readers_writers/reader_writer.c
@@ -22,21 +22,21 @@ OS_STK writer_stk[TASK_STACKSIZE];
 unsigned char num_readers = 0;
 
 void elipsis(){
-	INT8U i;
-	for(i=0; i<3; i++){
-		printf(".");
-		OSTimeDlyHMSM(0,0,1,0);
-	}
+    INT8U i;
+    for(i=0; i<3; i++){
+        printf(".");
+        OSTimeDlyHMSM(0,0,1,0);
+    }
 }
 
 void reader(void *pdata){
-	INT8U return_code;
+    INT8U return_code;
 
-	while(true)
-	{
+    while(true)
+    {
 
         // request access to the 'num_readers' variable
-		OSSemPend(mutex_num_readers, 0, &return_code);
+        OSSemPend(mutex_num_readers, 0, &return_code);
 
         // if we are the first reader request exclusive access for the readers
         if (num_readers == 0)
@@ -70,63 +70,63 @@ void reader(void *pdata){
         OSSemPost(mutex_num_readers);
         
         OSTimeDlyHMSM(0,0,1,0);
-	}
+    }
 }
 
 void writer(void *pdata){
-	INT8U return_code;
+    INT8U return_code;
 
-	while(true){
+    while(true){
 
         // request exclusive access
-		OSSemPend(mutex_access, 0 , &return_code);
+        OSSemPend(mutex_access, 0 , &return_code);
 
-		if(book_mark == WORDS_IN_BOOK -1){
-			book_mark = 0;
-		}
+        if(book_mark == WORDS_IN_BOOK -1){
+            book_mark = 0;
+        }
 
-		book[book_mark][0] = pangram[book_mark][0];
-		printf("\e[1;31mWriter: %s \n", book[book_mark][0]);
+        book[book_mark][0] = pangram[book_mark][0];
+        printf("\e[1;31mWriter: %s \n", book[book_mark][0]);
 
-		book_mark += 1;
+        book_mark += 1;
 
         // no longer need exclusive access
         OSSemPost(mutex_access);
 
         OSTimeDlyHMSM(0,0,1,0);
-	}
+    }
 }
 
 
 void  reader_writer_init()
 {
-	INT8U return_code = OS_NO_ERR;
-	book_mark = 0;
+    INT8U return_code = OS_NO_ERR;
+    book_mark = 0;
 
-	//initialize pangram
-	pangram[0][0] = "A\0";
-	pangram[1][0] = "quick\0";
-	pangram[2][0] = "brown\0";
-	pangram[3][0] = "fox\0";
-	pangram[4][0] = "jumps\0";
-	pangram[5][0] = "over\0";
-	pangram[6][0] = "the\0";
-	pangram[7][0] = "lazy\0";\
-	pangram[8][0] = "dog\0";
+    //initialize pangram
+    pangram[0][0] = "A\0";
+    pangram[1][0] = "quick\0";
+    pangram[2][0] = "brown\0";
+    pangram[3][0] = "fox\0";
+    pangram[4][0] = "jumps\0";
+    pangram[5][0] = "over\0";
+    pangram[6][0] = "the\0";
+    pangram[7][0] = "lazy\0";\
+    pangram[8][0] = "dog\0";
 
-	//create writer
-	return_code = OSTaskCreate(writer, NULL, (void*)&writer_stk[TASK_STACKSIZE-1], WRITER_PRIO);
-	alt_ucosii_check_return_code(return_code);
+    //create writer
+    return_code = OSTaskCreate(writer, NULL, (void*)&writer_stk[TASK_STACKSIZE-1], WRITER_PRIO);
+    alt_ucosii_check_return_code(return_code);
 
-	//create reader
-	return_code = OSTaskCreate(reader, (void*)1, (void*)&reader_1_stk[TASK_STACKSIZE-1], READER_1_PRIO);
-	alt_ucosii_check_return_code(return_code);
+    //create reader
+    return_code = OSTaskCreate(reader, (void*)1, (void*)&reader_1_stk[TASK_STACKSIZE-1], READER_1_PRIO);
+    alt_ucosii_check_return_code(return_code);
 
-	return_code = OSTaskCreate(reader, (void*)2, (void*)&reader_2_stk[TASK_STACKSIZE-1], READER_2_PRIO);
-	alt_ucosii_check_return_code(return_code);
+    return_code = OSTaskCreate(reader, (void*)2, (void*)&reader_2_stk[TASK_STACKSIZE-1], READER_2_PRIO);
+    alt_ucosii_check_return_code(return_code);
 
-	return_code = OSTaskCreate(reader, (void*)3, (void*)&reader_3_stk[TASK_STACKSIZE-1], READER_3_PRIO);
-	alt_ucosii_check_return_code(return_code);
+    return_code = OSTaskCreate(reader, (void*)3, (void*)&reader_3_stk[TASK_STACKSIZE-1], READER_3_PRIO);
+    alt_ucosii_check_return_code(return_code);
 }
 
 
@@ -140,11 +140,11 @@ int main (int argc, char* argv[], char* envp[])
 
     printf("Init..\n");
 
-	reader_writer_init();
+    reader_writer_init();
     
     printf("Starting..\n");
 
-	OSStart();
+    OSStart();
 
-	return 0;
+    return 0;
 }
